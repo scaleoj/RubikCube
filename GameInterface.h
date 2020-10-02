657c6bf5df09c4918ff915301e564b49c8ca28a7
Johannes H <johannesherfurth@web.de>
2020-10-02 18:12:59 +0200

    Keyboard Movement Done

--- GameInterface.h
+++ GameInterface.h
@@ -1,8 +1,10 @@
 #pragma once
+struct GLFWwindow;
 class GameInterface
 {
 public:
 	virtual void Initialize() {};
+	virtual void Initialize(GLFWwindow* window) { Initialize(); }
 
 	virtual void Update(double deltaTime) {};
 	virtual void Render(float aspectRatio) {};
